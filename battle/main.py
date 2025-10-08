f2eac98ceefa120a105ac5afe08b514742aba57e
Barley-1 <65284049+Barley-1@users.noreply.github.com>
2025-10-08 20:12:22 +0200

    Update main.py

--- battle/main.py
+++ battle/main.py
@@ -4,8 +4,11 @@ import dice
 import ship
 
 d = dice.Dice(10)
-ship1 = ship.Ship("Destroyer", 100, 20, 18, d)
-ship2 = ship.Ship("Cruiser", 100, 15, 22, d)
+def get_status():
+    return f'ship.Ship.get_status()'
+  
+ship1 = ship.Ship("Destroyer", 100, 20, 18, d, get_status())
+ship2 = ship.Ship("Cruiser", 100, 15, 22, d, get_status())
 
 ship1.attack_roll(ship2)
 print(ship1.get_status())
